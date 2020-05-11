675ddfec66dd4a117107cbcb160f9e7d9a44aa76
Alexey <volkov.aa@phystech.edu>
2020-05-11 08:51:38 +0300

    Created project

---
+++ cmake-build-debug/CMakeFiles/AlphaBlending.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AlphaBlending"
+  "AlphaBlending.pdb"
+  "CMakeFiles/AlphaBlending.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlphaBlending.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
